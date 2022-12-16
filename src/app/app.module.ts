2f0c2a4b6f05e442a765936153fa862a846f3220
Farahhamed <farah2011302@miuegypt.edu.eg>
2022-12-17 00:14:34 +0200

    added signup and payment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ProductsComponent } from './products/products.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PaymentComponent } from './payment/payment.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     ProductsComponent,
     MenuComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    PaymentComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
