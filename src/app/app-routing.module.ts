8090a418de47e9e4d6acd38643243b78b5ae8f91
nancy4hany <nancyhany8@gmail.com>
2022-12-17 19:38:15 +0200

    slving conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from 'src/app/home/home.component';
 import { MenuComponent } from 'src/app/menu/menu.component';
+import { HeaderComponent } from 'src/app/header/header.component';
 const routes: Routes = [
-
+{path:'',redirectTo:'home', pathMatch: 'full' },
+{path: 'header', component: HeaderComponent},
 { path: 'home', component: HomeComponent },
 { path: 'menu', component: MenuComponent }
 ];
