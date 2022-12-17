088243b16222991735a191d08044d240e646a57c
nancy4hany <nancyhany8@gmail.com>
2022-12-18 00:30:25 +0200

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,7 @@ import { LoginPageComponent} from 'src/app/login-page/login-page.component';
 import { SignupComponent} from 'src/app/signup/signup.component';
 import{ ContactUsComponent}from 'src/app/contact-us/contact-us.component';
 import{ AboutUsComponent}from 'src/app/about-us/about-us.component';
-<<<<<<< HEAD
 import{ PaymentComponent}from 'src/app/payment/payment.component';
-=======
-import{ CartComponent}from 'src/app/cart/cart.component';
-import { ProductsComponent } from './products/products.component';
->>>>>>> refs/remotes/origin/main
 const routes: Routes = [
 {path:'',redirectTo:'home', pathMatch: 'full' },
 {path: 'header', component: HeaderComponent},
@@ -22,14 +17,7 @@ const routes: Routes = [
 { path: 'menu', component: MenuComponent },
 {path: 'contactus', component:  ContactUsComponent},
 {path: 'aboutus', component:  AboutUsComponent},
-<<<<<<< HEAD
 {path: 'payment', component:  PaymentComponent},
-=======
-{path: 'login', component: LoginPageComponent},
-{path: 'cart', component: CartComponent},
-{path: 'products', component:ProductsComponent},
-{path: 'signup', component: SignupComponent}
->>>>>>> refs/remotes/origin/main
 ];
 
 @NgModule({
